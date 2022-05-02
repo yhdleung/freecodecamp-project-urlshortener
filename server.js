1487fa5bf3fdd7cb41887205ce77f22c13802b26
Dick Leung <yhd.leung@gmail.com>
2022-05-02 13:42:50 +0800

    implement API endpoints to store and read posted urls

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology:true});
+console.log(mongoose.connection.readyState);
+const urlSchmea = new mongoose.Schema({ 
+  url: {type: String},
+});
+let UrlModel = mongoose.model('urls', urlSchmea);
 
 app.use(cors());
 
@@ -14,10 +22,43 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.use(bodyParser.urlencoded({extended: false}))
+
+// API endpoints
+app.post('/api/shorturl', function(req, res) {
+  // verify submitted URL
+  const dns = require('dns')
+  try {
+    submitted_url = new URL(req.body.url)
+  } catch (error) {
+    res.json({"error": 'invalid url'});
+  }
+  dns.lookup(submitted_url.hostname, (err, address, family) => {
+    if(err){
+      res.json({error: 'invalid url'});
+    }else{
+      // save the URL
+      let newUrl = new UrlModel({url: submitted_url.href});
+      newUrl.save((err,data)=>{
+        if (err) return console.error(err);
+        res.json({
+          "original_url": data.url,
+          "short_url": data.id
+        })
+      })
+    }
+  });
+})
+
+app.get('/api/shorturl/:id', (req, res)=>{
+  UrlModel.findById(req.params.id, (err, data)=>{
+    if(!data){
+      res.json({error: 'invalid short url'});
+    }else{
+      res.redirect(data.url);
+    }
+  })
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
